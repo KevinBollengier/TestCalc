893b3ae80ecbae7cd11fae43dea4433569b6cdf7
Kevin Bollengier <Kevbollengier@gmail.com>
2017-09-11 13:23:18 -0500

    Testcalc works

---
+++ testCalc/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number1;
+    int number2;
+    int addition;
+    printf("Enter value 1:...\n");
+    scanf("%i",&number1);
+    printf("Enter value 2:...\n");
+    scanf("%i",&number2);
+
+    addition = number1 + number2;
+    printf("%d",addition);
+
+    return 0;
+}
