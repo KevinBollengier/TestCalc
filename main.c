ffd727466659ce2a7de996ce20a745fa0edc006a
Kevin Bollengier <Kevbollengier@gmail.com>
2017-09-11 13:39:04 -0500

    removed printf text

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main()
     int number1;
     int number2;
     int addition;
-    printf("Enter value 1:...\n");
+    //printf("Enter value 1:...\n");
     scanf("%i",&number1);
-    printf("Enter value 2:...\n");
+    //printf("Enter value 2:...\n");
     scanf("%i",&number2);
 
     addition = number1 + number2;
